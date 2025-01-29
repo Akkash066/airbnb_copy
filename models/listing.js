1b7426d8c920aa1372bd830c584b13946e3400ea
Akash <akashk494233@gmail.com>
2025-01-29 11:15:23 +0530

    validation for schema

--- models/listing.js
+++ models/listing.js
@@ -14,7 +14,10 @@ const listingSchema = new mongoose.Schema({
              v === ""
          ? "https://images.unsplash.com/photo-1680964504010-a2883187fcf2?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
     },
-    price: Number,
+    price: {
+       type: Number,
+       required: true,
+    },
     location: String,
     country: String,
 });
